acc8dc0ed1c4bc4a1ed3d3bf1e651b1bcf244b47
Sérgio Ramos <mail@sergioramos.me>
2018-02-20 23:50:25 +0000

    fix redirect

--- src/index.js
+++ src/index.js
@@ -8,56 +8,69 @@ const { ServerStyleSheet } = require('styled-components');
 const { default: Root } = require('./root');
 const { default: Scripts } = require('./scripts');
 
-module.exports = ({
-  indexFile,
-  getState
-}) => {
+module.exports = ({ indexFile, getState }) => {
   const html = readFileSync(indexFile, 'utf-8');
   const [pre, post] = html.split(/<div id="root"><\/div>/i);
+
+  const end = (res, props) => {
+    const prepost = renderToString(React.createElement(Scripts, props));
+    res.end(`${prepost}${post}`);
+  };
 
   return async (request, response, View) => {
     const { req, res } = request.raw;
 
     res.write(pre);
 
-    const { theme, createClient, createStore } = getState(request, response);
+    let apolloClient;
+    let routerContext;
+    let reduxStore;
+    let sheet;
+    let root;
 
-    const location = req.url;
-    const routerContext = {};
-    const apolloClient = createClient({ ssrMode: true });
-    const reduxStore = createStore();
-    const sheet = new ServerStyleSheet();
+    try {
+      const { theme, createClient, createStore } = getState(request, response);
 
-    const _root = React.createElement(
-      Root,
-      {
-        location,
-        routerContext,
-        reduxStore,
-        apolloClient,
-        sheet: sheet.instance,
-        theme
-      },
-      React.createElement(View)
-    );
+      const location = req.url;
+      routerContext = {};
+      apolloClient = createClient({ ssrMode: true });
+      reduxStore = createStore();
+      sheet = new ServerStyleSheet();
 
-    await getDataFromTree(_root);
-    const root = sheet.collectStyles(_root);
+      const _root = React.createElement(
+        Root,
+        {
+          location,
+          routerContext,
+          reduxStore,
+          apolloClient,
+          sheet: sheet.instance,
+          theme
+        },
+        React.createElement(View)
+      );
+
+      await getDataFromTree(_root);
+      root = sheet.collectStyles(_root);
+    } catch (err) {
+      console.log(err);
+      end(res, { redirect: `http://${req.headers.host}/~server-error` });
+      return res;
+    }
 
     const stream = sheet.interleaveWithNodeStream(renderToNodeStream(root));
 
+    stream.on('error', err => console.error(err));
     stream.pipe(res, { end: false });
-    stream.on('end', () => {
-      const prepost = renderToString(
-        React.createElement(Scripts, {
-          apolloState: apolloClient.extract(),
-          reduxState: reduxStore.getState(),
-          redirect: routerContext.url
-        })
-      );
 
-      res.end(`${prepost}${post}`);
-    });
+    stream.on('end', () =>
+      end(res, {
+        apolloState: apolloClient.extract(),
+        reduxState: reduxStore.getState(),
+        redirect:
+          routerContext.url && `http://${req.headers.host}${routerContext.url}`
+      })
+    );
 
     return res;
   };
--- src/scripts.js
+++ src/scripts.js
@@ -3,27 +3,28 @@ import React, { Fragment } from 'react';
 export default ({ apolloState, reduxState, redirect }) => (
   <Fragment>
     {redirect ? (
+      <meta httpEquiv="refresh" content={`0; url=${redirect}`} />
+    ) : null}
+
+    {apolloState ? (
       <script
-        dangerouslySetInnerHTML={{ __html: `window.location = ${redirect}` }}
+        dangerouslySetInnerHTML={{
+          __html: `window.__APOLLO_STATE__=${JSON.stringify(
+            apolloState
+          ).replace(/</g, '\\u003c')};`
+        }}
       />
     ) : null}
 
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `window.__APOLLO_STATE__=${JSON.stringify(apolloState).replace(
-          /</g,
-          '\\u003c'
-        )};`
-      }}
-    />
-
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `window.__REDUX_STATE__=${JSON.stringify(reduxState).replace(
-          /</g,
-          '\\u003c'
-        )};`
-      }}
-    />
+    {reduxState ? (
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.__REDUX_STATE__=${JSON.stringify(reduxState).replace(
+            /</g,
+            '\\u003c'
+          )};`
+        }}
+      />
+    ) : null}
   </Fragment>
 );
